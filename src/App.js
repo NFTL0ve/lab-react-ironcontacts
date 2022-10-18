5b40ea259a44b9a5c3ee0552f3aaab3d71900620
NFTL0ve <mbrandtk14@gmail.com>
2022-10-19 01:32:50 +0200

    first iterations, styling not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import objContacts from "./contacts.json";
+import { useState} from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [contacts, setContacts] = useState(objContacts.slice(0,5))
+  const [contactsLength, setContactsLength] = useState(4)
+
+
+function addRandom(){
+  let contactArray = [...objContacts];
+const randomContact = Math.floor(Math.random()*(contactArray.length-5)+5)
+contactArray.push(contactArray[randomContact]) 
+setContactsLength(contactsLength+1)
+setContacts(contactArray.slice(0, contactsLength))
+};
+return (
+  <div className="App">
+  <h1>IronContacts</h1>
+  <button onClick={function(){addRandom()}}>Add Random Contact</button>
+  <table>
+    <ul>
+      <li>Picture</li>
+      <li>Name</li>
+      <li>Popularity</li>
+      <li>Won an Oscar</li>
+      <li>Won an Emmy</li>
+    </ul>
+
+    {
+      contacts.map(objInfo => {
+            return(
+              <tr>
+                <td><img src={objInfo.pictureUrl} alt='pictureImg'/></td>
+                <td>{objInfo.name}</td>
+                <td>{Math.round(objInfo.popularity*100)/100}</td>
+                <td>{objInfo.wonOscar + "🏆"}</td>
+                <td>{objInfo.wonEmmy + "🏆"}</td>
+              </tr>
+            )
+      })
+    }
+  </table>
+  </div>
+);
+  }
+
+
+  export default App
